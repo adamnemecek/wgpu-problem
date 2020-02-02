f0c29ee1d4490c28512b18b1260d83f02002b0f0
Adam Nemecek <adamnemecek@gmail.com>
2020-02-02 15:50:18 -0800

    stuff

--- src/window.rs
+++ src/window.rs
@@ -1,13 +1,34 @@
-use {
-    crate::Device
+
+use winit::{
+    dpi::PhysicalPosition,
+    // event::{ElementState, Event, ModifiersState, MouseScrollDelta, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop, EventLoopWindowTarget},
+    window::{CursorIcon, WindowBuilder},
 };
 
+use crate::{
+    Device
+};
+
+
+
 pub struct Window {
-    device: Device
+    pub window: winit::window::Window,
+    pub device: Device,
 }
 
 impl Window {
-    pub fn new() -> Self {
-        todo!()
+    pub fn new(event_loop: &EventLoop<()>) -> Self {
+        let window = WindowBuilder::new()
+            .with_title("ngrid")
+            .build(&event_loop).expect("Failed to create window");
+
+        let device = Device::new(&window);
+
+        Self {
+            window,
+            device
+        }
     }
+
 }
