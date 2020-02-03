55ef41fdf18799c564dd4b500b69aefa0e5c45cf
Adam Nemecek <adamnemecek@gmail.com>
2020-02-02 16:08:30 -0800

    stuff

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,12 @@
 
-use crate::{
-    Window,
+use {
+    crate::{
+        Window,
+    },
     winit::{
         dpi::PhysicalPosition,
         event::{ElementState, Event, ModifiersState, MouseScrollDelta, VirtualKeyCode, WindowEvent},
         event_loop::{ControlFlow, EventLoop, EventLoopWindowTarget},
-        window::{CursorIcon, WindowBuilder},
     }
 };
 
@@ -18,6 +19,22 @@ impl App {
         Self { window: Window::new(event_loop) }
     }
 
+
+    pub fn get_next_texture<'a>(&'a mut self) -> wgpu::SwapChainOutput<'a> {
+        self.window.device.swap_chain.get_next_texture()
+    }
+
+    pub fn create_command_encoder(&mut self) -> wgpu::CommandEncoder {
+        self.window.device.device.create_command_encoder(&wgpu::CommandEncoderDescriptor { todo: 0 })
+        // todo!()
+    }
+
+    // pub fn redraw<'a>(&'a mut self, frame: &'a mut wgpu::SwapChainOutput) {
+    //     let frame = self.get_next_texture();
+    //     let encoder = self.create_command_encoder();
+
+    // }
+
 }
 
 pub fn app() {
@@ -25,20 +42,20 @@ pub fn app() {
     let mut app = App::new(&event_loop);
 
     event_loop.run(move |event, _, control_flow| match event {
-
-            Event::RedrawRequested(_) => {
-
-
-                // let mut frame = app.get_next_texture();
-
-                // app.redraw(&mut frame);
-                // // {
-
-                // let encoder = app.create_command_encoder();
-                // }
+        _ => ()
+            // Event::RedrawRequested(_) => {
 
 
-            },
+            //     // let mut frame = app.get_next_texture();
+
+            //     // app.redraw(&mut frame);
+            //     // // {
+
+            //     // let encoder = app.create_command_encoder();
+            //     // }
+
+
+            // },
 
 
         });
